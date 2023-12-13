8aa480165eda48cb88d4b33984dc108c6ae78af9
Filip Halapir <brokula444@gmail.com>
2023-12-13 21:59:52 +0100

    landing changed a bit, final

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <div className="md:py-32 py-12">
+    <div className="md:pt-32 pt-12">
       <div className="flex lg:flex-row flex-col lg:items-center">
         <div className="w-full max-w-xl mx-auto lg:order-1 lg:w-1/2">
           <h1 className="text-4xl font-bold md:text-6xl xl:text-7xl">Where dreams meet the road</h1>
@@ -13,19 +13,15 @@ const LandingPage = () => {
           </p>
           <div className="mt-6 flex items-center gap-2">
             <Link target="_blank" aria-label="linkedin" rel="noreferrer" to={"https://www.linkedin.com/in/filip-halapir/"}>
-              <Button>LinkedIn</Button>
+              <Button className="px-10">LinkedIn</Button>
             </Link>
             <Link target="_blank" aria-label="github" rel="noreferrer" to={"https://github.com/fhala03"}>
-              <Button variant={"secondary"}>Github</Button>
+              <Button className="bg-[#ff846c] text-background hover:bg-[#f77d65] px-10">Github</Button>
             </Link>
           </div>
         </div>
         <div className="w-full h-64 lg:mt-0 mt-10 lg:order-2 lg:w-1/2 lg:h-auto">
-          <img
-            className="h-auto w-full rounded-md object-cover max-w-lg mx-auto"
-            src="/car.svg"
-            alt="Illustration"
-          ></img>
+          <img className="h-auto w-full rounded-md object-cover max-w-lg mx-auto" src="/car.svg" alt="Illustration"></img>
         </div>
       </div>
     </div>
